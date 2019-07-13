3df24f3da5bbb378d147f9c9e99312b5537db7df
Nan Chen <nanx2483@hotmail.com>
2019-07-14 01:23:30 +0200

    Add LogHelper

---
+++ MAP.OA.Common/LogHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MAP.OA.Common
+{
+    public class LogHelper
+    {
+        public static Queue<string> ExceptionStringQueue = new Queue<string>();
+
+        static LogHelper()
+        {
+            // 从队列中获取错误信息写到 日志文件中
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                lock (ExceptionStringQueue)
+                {
+                    string str = ExceptionStringQueue.Dequeue();
+                    // 把异常写到日志中
+                }
+            });
+        }
+
+        public static void WriteLog(string exceptionText)
+        {
+            lock(ExceptionStringQueue)
+            {
+                ExceptionStringQueue.Enqueue(exceptionText);
+            }
+        }
+    }
+}
